44677394ce368abd10e8b9f4eda43b03179ad1fb
RobertMisch <robertmisch94@gmail.com>
2020-07-08 19:09:03 -0400

    finished no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,17 @@
 def no_dups(s):
-    # Your code here
-
+    #alternatively we could use a set here instead of a dict, but were practicing the latter
+    unique_words={}
+    result=''
+    s = s.split()
+    for word in s:
+        if word not in unique_words:
+            unique_words[word]=True
+    for k,v in unique_words.items():
+        result+=' ' + k
+    if result == '':
+        return ''
+    else:
+        return result[1:]
 
 
 if __name__ == "__main__":
